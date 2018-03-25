70f12cb2d0f0eae8421e2071dd6d4770b4e1ca05
wangyong <yundan_wy@126.com>
2018-03-25 23:25:43 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # springboot-demo
 springboot的一个小例子
+
+## 项目结构
+common 公共组件
+domain 数据model,服务接口定义
+service 服务接口实现
+web 对外提供服务
+
+## 技术栈
---
+++ web/src/main/java/com/springboot/demo/web/WebApplication.java
@@ -0,0 +1,18 @@
+package com.springboot.demo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ *
+ * @author wangyong
+ * @date 2018年03月25日
+ */
+@SpringBootApplication
+public class WebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebApplication.class, args);
+  }
+}
